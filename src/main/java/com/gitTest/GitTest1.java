eda70a8c9a06ac2262faf9349150f7a4c5e131ab
zh <1677654285@qq.com>
2022-08-09 11:02:51 +0800

    push commit

--- src/main/java/com/gitTest/GitTest1.java
+++ src/main/java/com/gitTest/GitTest1.java
@@ -8,5 +8,6 @@ public class GitTest1 {
         System.out.println("hot-fix test");
         System.out.println("hot-fix test2222");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
