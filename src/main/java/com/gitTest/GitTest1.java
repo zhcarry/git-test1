496524a4306dd43b7c258b61f6574d5c90dfa119
zh <1677654285@qq.com>
2022-08-09 10:33:00 +0800

    hot-fix commit3

--- src/main/java/com/gitTest/GitTest1.java
+++ src/main/java/com/gitTest/GitTest1.java
@@ -6,5 +6,6 @@ public class GitTest1 {
         System.out.println("111");
         System.out.println("22");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2222");
     }
 }
