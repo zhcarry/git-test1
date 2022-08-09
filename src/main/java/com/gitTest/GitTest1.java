852e0af8ab077505a7531825359d9b3364ab44c5
zhcarry <110659214+zhcarry@users.noreply.github.com>
2022-08-09 11:05:48 +0800

    Update GitTest1.java

--- src/main/java/com/gitTest/GitTest1.java
+++ src/main/java/com/gitTest/GitTest1.java
@@ -9,5 +9,6 @@ public class GitTest1 {
         System.out.println("hot-fix test2222");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
